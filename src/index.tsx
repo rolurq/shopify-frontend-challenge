a45b3aa2d4c0d5f3796324b1ab0674d85d019ea9
Rolando Urquiza <rolurquiza@gmail.com>
2019-01-12 17:43:53 -0500

    Add app icons

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,6 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 registerServiceWorker();
