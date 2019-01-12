43cdbd4d6a482352161655a9b0dc4454cec6ae8c
Rolando Urquiza <rolurquiza@gmail.com>
2019-01-11 21:38:47 -0500

    Provide store to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import App from './App';
+
 import './index.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
