013ff7902c1ab3387d4d2e23c2693691ed969ef5
Rolando Urquiza <rolurquiza@gmail.com>
2019-01-12 19:00:14 -0500

    Add searching flag to state

--- src/reducers/search.ts
+++ src/reducers/search.ts
@@ -1,23 +1,32 @@
-import { IActions } from '../actions';
+import { AppActions } from '../actions';
 import { SearchResult } from '../models';
 
 export interface SearchState {
+  searching: boolean;
   results?: SearchResult[];
 }
 
 export default function search(
-  state: SearchState = { results: undefined },
+  state: SearchState = { searching: false, results: undefined },
   action: AppActions,
 ): SearchState {
   switch (action.type) {
     case 'SEARCH_SUBMIT':
       return {
         ...state,
+        searching: true,
       };
     case 'SEARCH_RESULTS':
       return {
         ...state,
         results: action.results,
+        searching: false,
+      };
+    case 'SEARCH_CLEAR':
+      return {
+        ...state,
+        results: undefined,
+        searching: false,
       };
     default:
       return state;
