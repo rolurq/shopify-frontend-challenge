adca7dabe097cba788b61d028a42a308df774ca1
Rolando Urquiza <rolurquiza@gmail.com>
2019-01-14 15:06:25 -0500

    Render favorites as a SearachResultList

--- src/components/Favourites/Favourites.tsx
+++ src/components/Favourites/Favourites.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import './Favourites.css';
 
-import SearchResult from '../SearchResult';
+import { SearchResult } from '../../models';
+// using the disconnected component to pass the list of favourites
+// as a result list
+import SearchResultList from '../SearchResultList/SearchResultList';
 
-export default function Favourites() {
-  return (
-    <footer className="Favourites">
-      <h2 className="Favourites-title">Favourites</h2>
-      <SearchResult
-        title="Blue Bin (auto-related plastic bottle)"
-        body=" &lt;li&gt;Place this empty and rinsed item in the&amp;nbsp;&lt;strong&gt;Blue Bin&lt;/strong&gt;.&lt;/li&gt;
-        &lt;/ul&gt;"
-        favourite={true}
-      />
-    </footer>
-  );
+export interface StateProps {
+  favourites: IterableIterator<SearchResult>;
+  empty: boolean;
+}
+
+export default class Favourites extends React.PureComponent<StateProps> {
+  render() {
+    const { empty, favourites } = this.props;
+    return empty ? null : (
+      <section className="Favourites">
+        <footer className="Favourites-container">
+          <h2 className="Favourites-title">Favourites</h2>
+          <SearchResultList results={Array.from(favourites)} />
+        </footer>
+      </section>
+    );
+  }
 }
--- src/components/Favourites/index.ts
+++ src/components/Favourites/index.ts
@@ -1,1 +1,22 @@
-export { default } from './Favourites';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+
+import { AppState } from '../../reducers';
+import Favourites, { StateProps } from './Favourites';
+
+function mapStateToProps(state: AppState): StateProps {
+  const { selection } = state.favourites;
+  return {
+    empty: selection.size === 0,
+    favourites: selection.values(),
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {};
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Favourites);
